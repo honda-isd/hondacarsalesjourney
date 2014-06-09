1b09ef7a2e75d66bf28d6848830d2d70c8517e9b
rajrrhadhhash <raja.khurram20@gmail.com>
2014-06-09 13:02:07 +0100

    angular genartor installed

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hondacarsalesjourneyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hondacarsalesjourneyApp
+ */
+angular.module('hondacarsalesjourneyApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
